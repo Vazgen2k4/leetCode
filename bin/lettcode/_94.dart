66e5e385406850f95e40c3b421267666fd8c1a2c
Vazgen <Vazgen2004proweb@gmail.com>
2024-08-30 20:53:52 +0200

    Compleate solution number 94 (Binary Tree)

---
+++ bin/lettcode/_94.dart
@@ -0,0 +1,34 @@
+void main(List<String> args) {
+  
+}
+
+
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+  
+  @override
+  String toString() {
+    return ' This: $hashCode\n Value: $val\n Left: ${left.hashCode}\n Right: ${right.hashCode}\n';
+  }
+  
+}
+
+class Solution {
+  List<int> inorderTraversal(TreeNode? root) {
+    if (root == null) {
+      return [];
+    }
+    
+    if(root.left == null && root.right == null) {
+      return [root.val];
+    }
+    
+    final leftList = inorderTraversal(root.left);
+    final rightList = inorderTraversal(root.right);
+    
+    return [...leftList, root.val ,...rightList];
+  }
+}
